045bdb87d65d791ac09afdfdb7ce75bb9c05aa2e
NoughtQ <3470273548@qq.com>
2024-08-27 18:42:57 +0800

    Deployed 6e8e001 with MkDocs version: 1.6.0

--- javascripts/anchor.js
+++ javascripts/anchor.js
@@ -1,14 +1,19 @@
 // 点击链接打开新的标签页
-
 document.querySelectorAll('a').forEach(link => {
-  link.addEventListener('click', event => {
-      const href = link.getAttribute('href');
-      const isSameOrigin = new URL(href, window.location.origin).origin === window.location.origin;
-
-      if (isSameOrigin) {
-          link.target = '_self';
-      } else {
-          link.target = '_blank';
-      }
+    link.addEventListener('click', event => {
+        const href = link.getAttribute('href');
+        
+        // 忽略带 # 的链接
+        if (href.startsWith('#')) {
+            return;
+        }
+  
+        const isSameOrigin = new URL(href, window.location.origin).origin === window.location.origin;
+  
+        if (isSameOrigin) {
+            link.target = '_self';
+        } else {
+            link.target = '_blank';
+        }
+    });
   });
-});
