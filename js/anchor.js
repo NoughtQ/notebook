178cc5c970f72adcd9ec6a800c9389a1df1a0eec
NoughtQ <noughtq666@gmail.com>
2024-11-05 09:47:09 +0800

    Deployed 622e6b2 with MkDocs version: 1.6.1

--- js/anchor.js
+++ js/anchor.js
@@ -1,34 +1,38 @@
 // Written by ChatGPT 4o mini
 
 document.addEventListener('DOMContentLoaded', function () {
-  function processLinks() {
-      const links = document.querySelectorAll('a');
-      links.forEach(link => {
-          const href = link.getAttribute('href');
-          if (!href) return; // 忽略没有 href 属性的链接
-
-          try {
-              const linkUrl = new URL(href, window.location.href);
-              const baseUrl = new URL(window.location.href);
-	            const cleanLinkUrl = new URL(linkUrl.origin + linkUrl.pathname + linkUrl.search, baseUrl.origin);
-
-              if (cleanLinkUrl.origin === baseUrl.origin) {
-                  // 同源链接
-                  link.target = '_self';
-                  link.style.textDecoration = 'none'; // 确保没有下划线
-              } else {
-                  // 跨源链接
-                  link.target = '_blank';
-                  link.classList.add('cross-origin'); // 添加 cross-origin 类以设置下划线
-              }
-          } catch (error) {
-              // 如果 URL 解析失败，处理为跨源
-              link.target = '_blank';
-              link.classList.add('cross-origin'); // 添加 cross-origin 类以设置下划线
-          }
-      });
-  }
-
-  processLinks();
-});
+    function processLinks() {
+        const links = document.querySelectorAll('a');
+        links.forEach(link => {
+            const href = link.getAttribute('href');
+            if (!href) return; // 忽略没有 href 属性的链接
+  
+            // 忽略仅包含空格或不可见字符的链接
+            if (href.trim().length === 0) return;
+  
+            try {
+                const linkUrl = new URL(href, window.location.href);
+                const baseUrl = new URL(window.location.href);
+                const cleanLinkUrl = new URL(linkUrl.origin + linkUrl.pathname + linkUrl.search, baseUrl.origin);
+  
+                if (cleanLinkUrl.origin === baseUrl.origin) {
+                    // 同源链接
+                    link.target = '_self';
+                    link.style.textDecoration = 'none'; // 确保没有下划线
+                } else {
+                    // 跨源链接
+                    link.target = '_blank';
+                    link.classList.add('cross-origin'); // 添加 cross-origin 类以设置下划线
+                }
+            } catch (error) {
+                // 如果 URL 解析失败，处理为跨源
+                link.target = '_blank';
+                link.classList.add('cross-origin'); // 添加 cross-origin 类以设置下划线
+            }
+        });
+    }
+  
+    processLinks();
+  });
+  
   
